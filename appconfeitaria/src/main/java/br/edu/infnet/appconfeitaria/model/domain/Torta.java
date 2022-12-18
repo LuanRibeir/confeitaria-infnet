d2617e16b2edd8cd857c2a4ee6432417a0cbe277
LuanRibeir <luaneribeiroalmeida@gmail.com>
2022-12-18 15:40:31 -0300

    Add exception limite numero de caracteres torta

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/domain/Torta.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/domain/Torta.java
@@ -1,5 +1,6 @@
 package br.edu.infnet.appconfeitaria.model.domain;
 
+import br.edu.infnet.appconfeitaria.model.exceptions.NumeroDeCaracteresInvalidoException;
 import br.edu.infnet.appconfeitaria.model.exceptions.ValorZeradoException;
 
 public class Torta extends Doce{
@@ -8,11 +9,11 @@ public class Torta extends Doce{
     private boolean isDoce; //doce ou salgada
 
     public Torta(String codigo, String nome, String sabor, int kilo,
-                float valorKg, Boolean vegana, String decoracao, Boolean doce) throws ValorZeradoException{
-
+                float valorKg, Boolean vegana, String decoracao, Boolean doce)
+                throws ValorZeradoException, NumeroDeCaracteresInvalidoException{
         super(codigo, nome, sabor, kilo, valorKg);
         this.vegana = vegana;
-        this.decoracao = decoracao;
+        this.setDecoracao(decoracao);
         this.isDoce = doce;
     }
 
@@ -56,7 +57,11 @@ public class Torta extends Doce{
     public String getDecoracao() {
         return decoracao;
     }
-    public void setDecoracao(String decoracao) {
+    public void setDecoracao(String decoracao) throws NumeroDeCaracteresInvalidoException{
+        if (decoracao.length() > 140){
+            throw new NumeroDeCaracteresInvalidoException("[ERRO] Mantenha a decoração com no máximo de 140 characteres.");
+        }
+
         this.decoracao = decoracao;
     }
 
