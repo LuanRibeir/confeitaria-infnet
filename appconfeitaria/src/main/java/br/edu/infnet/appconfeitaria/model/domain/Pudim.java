4520091a9f855513c2be855ac785a9966c779af1
LuanRibeir <luaneribeiroalmeida@gmail.com>
2022-12-18 15:02:09 -0300

    Add atributos pudim

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/domain/Pudim.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/domain/Pudim.java
@@ -1,5 +1,18 @@
 package br.edu.infnet.appconfeitaria.model.domain;
 
 public class Pudim extends Doce{
+    private String tamanho;
+    private boolean lactose;
+    private int quantidade;
+
+    public Pudim(String codigo, String nome, String sabor, int kilo,
+                float valorKg, String tamanho, Boolean lactose, int quantidade) throws ValorZeradoException{
+                    
+        super(codigo, nome, sabor, kilo, valorKg);
+        this.tamanho = tamanho;
+        this.lactose = lactose;
+        this.quantidade = quantidade;
+    }
+
     
 }
