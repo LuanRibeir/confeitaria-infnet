3dab13bd52632aeb341b73b84a27fb73997c3b86
LuanRibeir <luaneribeiroalmeida@gmail.com>
2022-12-18 15:05:51 -0300

    Add testes de doce

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/tests/DoceTest.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/tests/DoceTest.java
@@ -1,7 +1,42 @@
 package br.edu.infnet.appconfeitaria.model.tests;
 
+import br.edu.infnet.appconfeitaria.model.domain.Bolo;
+import br.edu.infnet.appconfeitaria.model.domain.Pudim;
+import br.edu.infnet.appconfeitaria.model.domain.Torta;
+import br.edu.infnet.appconfeitaria.model.exceptions.NumeroDeCamadasInvalidoException;
+import br.edu.infnet.appconfeitaria.model.exceptions.ValorZeradoException;
+
 public class DoceTest {
     public static void main(String[] args) {
         
+        try {
+            Bolo boloQuadrado = new Bolo("codigoteste1", "bolo de formigueiro", "chocolate",
+                                    2, 25.0f, "quadrado", "chocolate", 2);
+
+            System.out.println(boloQuadrado);
+            System.out.println(boloQuadrado.calcularValorPorKilo());
+        } catch (ValorZeradoException | NumeroDeCamadasInvalidoException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            Pudim pudimP = new Pudim("codigoteste1", "Pudim", "chocolate",
+            2, 25.0f, "P", true, 1);
+
+            System.out.println(pudimP);
+            System.out.println(pudimP.calcularValorPorKilo());
+        } catch (ValorZeradoException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            Torta tortaSalgadaVegana = new Torta("codigoteste1", "Torta Salgada Vegana", "sabor",
+            2, 25.0f, true, "decoracao", false);
+
+            System.out.println(tortaSalgadaVegana);
+            System.out.println(tortaSalgadaVegana.calcularValorPorKilo());
+        } catch (ValorZeradoException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
