e34ce07a63f38c9f12fbb1c8f16b3905709ef469
LuanRibeir <luaneribeiroalmeida@gmail.com>
2022-12-18 01:02:09 -0300

    Add testes do cliente

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/tests/ClienteTest.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/tests/ClienteTest.java
@@ -1,7 +1,16 @@
 package br.edu.infnet.appconfeitaria.model.tests;
 
+import br.edu.infnet.appconfeitaria.model.domain.Cliente;
+
 public class ClienteTest {
     public static void main(String[] args) {
-        
+        Cliente cli0 = new Cliente("joe", "doe", 123456789);
+        System.out.println(cli0);
+
+        Cliente cli1 = new Cliente("joe", "doe", 123456789);
+        System.out.println(cli1);
+
+        Cliente cli2 = new Cliente("joe", "doe", 123456789);
+        System.out.println(cli2);
     }
 }
