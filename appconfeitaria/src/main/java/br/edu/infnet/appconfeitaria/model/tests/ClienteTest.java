14156a9a1d83f385f3ec01db29d23b44a1f1b9ed
LuanRibeir <luaneribeiroalmeida@gmail.com>
2022-12-18 15:05:45 -0300

    Add testes de cliente

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/tests/ClienteTest.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/tests/ClienteTest.java
@@ -1,16 +1,67 @@
 package br.edu.infnet.appconfeitaria.model.tests;
 
 import br.edu.infnet.appconfeitaria.model.domain.Cliente;
+import br.edu.infnet.appconfeitaria.model.exceptions.ClienteInvalidoException;
 
 public class ClienteTest {
     public static void main(String[] args) {
-        Cliente cli0 = new Cliente("joe", "doe", 123456789);
-        System.out.println(cli0);
 
-        Cliente cli1 = new Cliente("joe", "doe", 123456789);
-        System.out.println(cli1);
+        try {
+            Cliente cli0 = new Cliente("joe", "doe", 123456789);
+            System.out.println(cli0);
+        } catch (ClienteInvalidoException e) {
+            System.out.println(e.getMessage());
+        }
 
-        Cliente cli2 = new Cliente("joe", "doe", 123456789);
-        System.out.println(cli2);
+        try {
+            Cliente cli0 = new Cliente("joe", "doe", 123456789);
+            System.out.println(cli0);
+        } catch (ClienteInvalidoException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            Cliente cli0 = new Cliente("joe", "doe", 123456789);
+            System.out.println(cli0);
+        } catch (ClienteInvalidoException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            Cliente cli1 = new Cliente("joe", "doe", 123456789);
+            System.out.println(cli1);
+        } catch (ClienteInvalidoException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            Cliente cli2 = new Cliente("joe", "doe", 123456789);
+            System.out.println(cli2);
+        } catch (ClienteInvalidoException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            Cliente cli3 = new Cliente("joe", "doe", 0);
+            System.out.println(cli3);
+        } catch (ClienteInvalidoException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            Cliente cli4 = new Cliente("joe", null, 123456789);
+            System.out.println(cli4);
+        } catch (ClienteInvalidoException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            Cliente cli5 = new Cliente(null, "doe", 123456789);
+            System.out.println(cli5);
+        } catch (ClienteInvalidoException e) {
+            System.out.println(e.getMessage());
+        }
+
+
     }
 }
