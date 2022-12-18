2e2ff14b537a1a782887bc827d0385a55852bd67
LuanRibeir <luaneribeiroalmeida@gmail.com>
2022-12-18 15:00:20 -0300

    Add exception cliente

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/domain/Cliente.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/domain/Cliente.java
@@ -1,12 +1,26 @@
 package br.edu.infnet.appconfeitaria.model.domain;
 
+import br.edu.infnet.appconfeitaria.model.exceptions.ClienteInvalidoException;
+
 public class Cliente {
     private String nome;
     private String cpf;
     private int telefone;
 
-    public Cliente(String nome, String cpf, int telefone) {
+    public Cliente(String nome, String cpf, int telefone) throws ClienteInvalidoException{
         this.nome = nome;
+        if(nome == null){
+            throw new ClienteInvalidoException("[ERRO] Nome inválido!");
+        }
+
+        if(cpf == null){
+            throw new ClienteInvalidoException("[ERRO] cpf inválido!");
+        }
+
+        if(telefone == 0){
+            throw new ClienteInvalidoException("[ERRO] Numero inválido!");
+        }
+
         this.cpf = cpf;
         this.telefone = telefone;
     }
