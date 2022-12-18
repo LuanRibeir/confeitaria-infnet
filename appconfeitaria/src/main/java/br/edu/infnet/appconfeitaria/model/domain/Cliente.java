316a91b965e3db12b5dacae4f1e18b5f11c6fe4d
LuanRibeir <luaneribeiroalmeida@gmail.com>
2022-12-18 00:25:35 -0300

    Add atributos de cliente

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/domain/Cliente.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/domain/Cliente.java
@@ -1,5 +1,14 @@
 package br.edu.infnet.appconfeitaria.model.domain;
 
 public class Cliente {
+    private String nome;
+    private String cpf;
+    private int telefone;
+
+    public Cliente(String nome, String cpf, int telefone) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+    }
     
 }
