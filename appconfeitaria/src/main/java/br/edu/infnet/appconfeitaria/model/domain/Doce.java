d9a091a44adecb24dcd5300e6ab47fb7a3433b93
LuanRibeir <luaneribeiroalmeida@gmail.com>
2022-12-18 09:46:20 -0300

    Add atributos de doce

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/domain/Doce.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/domain/Doce.java
@@ -1,5 +1,19 @@
 package br.edu.infnet.appconfeitaria.model.domain;
 
 public abstract class Doce {
+    private String codigo;
+    private String nome;
+    private String sabor;
+    private int kilo;
+    private float valorKg;
+
+
+    public Doce(String codigo, String nome, String sabor, int kilo, float valorKg) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.sabor = sabor;
+        this.kilo = kilo;
+        this.valorKg = valorKg;
+    }
     
 }
