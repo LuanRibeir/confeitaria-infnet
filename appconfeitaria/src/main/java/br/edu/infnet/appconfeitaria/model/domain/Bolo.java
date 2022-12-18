62275f1c005b9424f1f13a48f5524260ebd38bbd
LuanRibeir <luaneribeiroalmeida@gmail.com>
2022-12-18 09:50:13 -0300

    Add getters & setters de bolo

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/domain/Bolo.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/domain/Bolo.java
@@ -14,4 +14,25 @@ public class Bolo extends Doce{
         this.saborCobertura = saborCobertura;
     }
     
+    public String getFormato() {
+        return formato;
+    }
+    public void setFormato(String formato) {
+        this.formato = formato;
+    }
+
+    public String getSaborCobertura() {
+        return saborCobertura;
+    }
+    public void setSaborCobertura(String saborCobertura) {
+        this.saborCobertura = saborCobertura;
+    }
+
+    public int getCamadas() {
+        return camadas;
+    }
+    public void setCamadas(int camadas) {
+        this.camadas = camadas;
+    }
+
 }
