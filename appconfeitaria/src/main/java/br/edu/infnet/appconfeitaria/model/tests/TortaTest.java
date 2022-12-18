4753ab08ac9e802c261817bbe7864b5b4299d450
LuanRibeir <luaneribeiroalmeida@gmail.com>
2022-12-18 15:06:09 -0300

    Add testes de torta

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/tests/TortaTest.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/tests/TortaTest.java
@@ -1,7 +1,39 @@
 package br.edu.infnet.appconfeitaria.model.tests;
 
+import br.edu.infnet.appconfeitaria.model.domain.Torta;
+import br.edu.infnet.appconfeitaria.model.exceptions.ValorZeradoException;
+
 public class TortaTest {
     public static void main(String[] args) {
+
+        try {
+            Torta tortaSalgadaVegana = new Torta("codigoteste1", "Torta Salgada Vegana", "sabor",
+            2, 25.0f, true, "decoracao", false);
+
+            System.out.println(tortaSalgadaVegana);
+            System.out.println(tortaSalgadaVegana.calcularValorPorKilo());
+        } catch (ValorZeradoException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            Torta tortaDoce = new Torta("codigoteste1", "Torta Salgada", "sabor",
+            2, 25.0f, false, "decoracao", true);
+    
+            System.out.println(tortaDoce);
+            System.out.println(tortaDoce.calcularValorPorKilo());
+        } catch (ValorZeradoException e) {
+            System.out.println(e.getMessage());
+        }
         
+        try {
+            Torta tortaDoceVegana = new Torta("codigoteste1", "Torta Salgada", "sabor",
+            2, 25.0f, true, "decoracao", true);
+    
+            System.out.println(tortaDoceVegana);
+            System.out.println(tortaDoceVegana.calcularValorPorKilo());
+        } catch (ValorZeradoException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
