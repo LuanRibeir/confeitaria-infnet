0bc259887a900358abaa5eaa488568f3033fd87f
LuanRibeir <luaneribeiroalmeida@gmail.com>
2022-12-18 00:58:45 -0300

    Add getters & setters de pedido

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/domain/Pedido.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/domain/Pedido.java
@@ -7,4 +7,35 @@ public class Pedido {
     private Cliente cliente;
     private List<Doce> doces;
     
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isWeb() {
+        return web;
+    }
+    public void setWeb(boolean web) {
+        this.web = web;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<Doce> getDoces() {
+        return doces;
+    }
+    public void setDoces(List<Doce> doces) {
+        this.doces = doces;
+    }
 }
