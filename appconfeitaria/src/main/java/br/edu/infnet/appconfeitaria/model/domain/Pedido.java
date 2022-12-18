22ddaadc481aefffcb88efb971a072c3502bc7cc
LuanRibeir <luaneribeiroalmeida@gmail.com>
2022-12-18 00:57:16 -0300

    Add atributos de pedido

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/domain/Pedido.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/domain/Pedido.java
@@ -1,5 +1,10 @@
 package br.edu.infnet.appconfeitaria.model.domain;
 
 public class Pedido {
+    private String descricao;
+    private boolean web;
+    private LocalDateTime data; 
+    private Cliente cliente;
+    private List<Doce> doces;
     
 }
