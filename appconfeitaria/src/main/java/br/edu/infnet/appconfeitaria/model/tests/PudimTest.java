1cfd7f3bfa31b471fb9478ad8dc081534160e938
LuanRibeir <luaneribeiroalmeida@gmail.com>
2022-12-18 15:06:03 -0300

    Add testes de pudim

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/tests/PudimTest.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/tests/PudimTest.java
@@ -1,7 +1,39 @@
 package br.edu.infnet.appconfeitaria.model.tests;
 
+import br.edu.infnet.appconfeitaria.model.domain.Pudim;
+import br.edu.infnet.appconfeitaria.model.exceptions.ValorZeradoException;
+
 public class PudimTest {
     public static void main(String[] args) {
-        
+        try {
+            Pudim pudimP = new Pudim("codigoteste1", "Pudim", "chocolate",
+            2, 25.0f, "P", true, 1);
+
+            System.out.println(pudimP);
+            System.out.println(pudimP.calcularValorPorKilo());
+        } catch (ValorZeradoException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            Pudim pudimSemLactoseM = new Pudim("codigoteste2", "Pudim", "chocolate", 2,
+            25.0f, "M", false, 2);
+
+            System.out.println(pudimSemLactoseM);
+            System.out.println(pudimSemLactoseM.calcularValorPorKilo());
+        } catch (ValorZeradoException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            Pudim pudimG = new Pudim("codigoteste3", "Pudim", "chocolate",
+            2, 25.0f, "G", true, 1);
+    
+            System.out.println(pudimG);
+            System.out.println(pudimG.calcularValorPorKilo());
+    
+        } catch (ValorZeradoException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
