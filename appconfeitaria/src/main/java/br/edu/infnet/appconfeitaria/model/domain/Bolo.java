a48dffd1a314d70fc41f049bd3bf334f00858e12
LuanRibeir <luaneribeiroalmeida@gmail.com>
2022-12-18 09:49:28 -0300

    Add atributos bolo

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/domain/Bolo.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/domain/Bolo.java
@@ -1,5 +1,17 @@
 package br.edu.infnet.appconfeitaria.model.domain;
 
 public class Bolo extends Doce{
+    private String formato;
+    private String saborCobertura;
+    private int camadas;
+
+    public Bolo(String codigo, String nome, String sabor, int kilo,
+                float valorKg, String formato, String saborCobertura, int camadas) {
+                    
+        super(codigo, nome, sabor, kilo, valorKg);
+        this.formato = formato;
+        this.camadas = camadas;
+        this.saborCobertura = saborCobertura;
+    }
     
 }
