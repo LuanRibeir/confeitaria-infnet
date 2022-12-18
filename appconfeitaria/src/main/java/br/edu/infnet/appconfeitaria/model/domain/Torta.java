a718d4558a328268aa056d3a039771c474ddf91e
LuanRibeir <luaneribeiroalmeida@gmail.com>
2022-12-18 15:04:03 -0300

    Add atributos torta

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/domain/Torta.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/domain/Torta.java
@@ -1,5 +1,17 @@
 package br.edu.infnet.appconfeitaria.model.domain;
 
 public class Torta extends Doce{
+    private boolean vegana;
+    private String decoracao;
+    private boolean isDoce; //doce ou salgada
+
+    public Torta(String codigo, String nome, String sabor, int kilo,
+                float valorKg, Boolean vegana, String decoracao, Boolean doce) throws ValorZeradoException{
+
+        super(codigo, nome, sabor, kilo, valorKg);
+        this.vegana = vegana;
+        this.decoracao = decoracao;
+        this.isDoce = doce;
+    }
     
 }
