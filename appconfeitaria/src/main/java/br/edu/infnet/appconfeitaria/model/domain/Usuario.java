764094e7d9799a3daabaccd7007828e190bd78db
LuanRibeir <luaneribeiroalmeida@gmail.com>
2023-02-09 17:46:20 -0300

    Add atributos de usuario

---
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/domain/Usuario.java
@@ -0,0 +1,39 @@
+package br.edu.infnet.appconfeitaria.model.domain;
+
+public class Usuario {
+    private String nome;
+    private String senha;
+    private String email;
+
+    public Usuario(String nome, String email){
+        this.setNome(nome);
+        this.setEmail(email);
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Bem vindo/a %s de e-mail %s!", this.nome, this.email);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
