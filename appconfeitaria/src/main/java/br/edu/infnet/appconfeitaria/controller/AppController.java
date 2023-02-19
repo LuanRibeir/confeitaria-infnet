125f6fe4922b83e7d569557b6cc4c573c252284d
LuanRibeir <luaneribeiroalmeida@gmail.com>
2023-02-19 17:00:47 -0300

    Change rota de app

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/controller/AppController.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/controller/AppController.java
@@ -3,12 +3,16 @@ package br.edu.infnet.appconfeitaria.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class AppController {
 
     @GetMapping(value = "/")
     public String telaIndex() {
+        return "redirect:/login";
+    }
+
+    @GetMapping(value = "/home")
+    public String telaHome() {
         return "index";
     }
 }
