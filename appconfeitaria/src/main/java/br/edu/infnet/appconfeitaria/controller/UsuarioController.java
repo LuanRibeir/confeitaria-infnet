bd9d5973a069e78ec6c254568d2c5c3623d6bc32
LuanRibeir <luaneribeiroalmeida@gmail.com>
2023-03-14 14:12:21 -0300

    Add hashmap & metodo de exclusao

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/controller/UsuarioController.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/controller/UsuarioController.java
@@ -1,10 +1,9 @@
 package br.edu.infnet.appconfeitaria.controller;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import br.edu.infnet.appconfeitaria.model.domain.Usuario;
@@ -13,7 +12,7 @@ import br.edu.infnet.appconfeitaria.model.repository.UsuarioRepository;
 
 @Controller
 public class UsuarioController {
-    private String alertaInclussaoSucesso;
+    private String msgAlerta;
 
     @GetMapping(value = "/usuario")
     public String telaCadastro() {
@@ -23,8 +22,8 @@ public class UsuarioController {
     @GetMapping(value = "/usuario/lista")
     public String telaLista(Model model) {
         model.addAttribute("usuarios", UsuarioRepository.obterLista());
-        model.addAttribute("mensagem", alertaInclussaoSucesso);
-        alertaInclussaoSucesso = null;
+        model.addAttribute("mensagem", msgAlerta);
+        msgAlerta = null;
 
         return "usuario/lista";
     }
@@ -33,7 +32,16 @@ public class UsuarioController {
     public String incluir(Usuario usuario) {
         UsuarioRepository.incluir(usuario);
 
-        alertaInclussaoSucesso = "Inclussão realizada com sucesso!";
+        msgAlerta = "Inclussão realizada!";
+
+        return "redirect:/usuario/lista";
+    }
+
+    @GetMapping(value = "/usuario/{id}/excluir")
+    public String excluir(@PathVariable Integer id) {
+        Usuario usuario = UsuarioRepository.excluir(id);
+
+        msgAlerta = "Exclussão realizada!";
 
         return "redirect:/usuario/lista";
     }
--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/repository/UsuarioRepository.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/repository/UsuarioRepository.java
@@ -1,23 +1,31 @@
 package br.edu.infnet.appconfeitaria.model.repository;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import br.edu.infnet.appconfeitaria.model.domain.Usuario;
 
 public class UsuarioRepository {
-    private static List<Usuario> lista = new ArrayList<Usuario>();
+    private static Integer id = 1;
+    private static Map<Integer,Usuario> mapaUsuario = new HashMap<Integer, Usuario>();
 
     public static boolean incluir(Usuario usuario) {
+        usuario.setId(id++);
+
         try {
-            lista.add(usuario);
+            mapaUsuario.put(usuario.getId(), usuario);
             return true;
         } catch (Exception e) {
             return false;
         }
     }
 
-    public static List<Usuario> obterLista(){
-        return lista;
+    public static Usuario excluir(Integer id) {
+        return mapaUsuario.remove(id);
+    }
+
+    public static Collection<Usuario> obterLista(){
+        return mapaUsuario.values();
     }
 }
