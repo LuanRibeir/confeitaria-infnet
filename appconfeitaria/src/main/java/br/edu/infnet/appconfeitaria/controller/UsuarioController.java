08a7a66a1c5c32f49fca7e23ea09fe003dc00834
LuanRibeir <luaneribeiroalmeida@gmail.com>
2023-03-14 11:50:37 -0300

    Add mensagem de inclussao de usuario

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/controller/UsuarioController.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package br.edu.infnet.appconfeitaria.controller;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -12,30 +13,27 @@ import br.edu.infnet.appconfeitaria.model.repository.UsuarioRepository;
 
 @Controller
 public class UsuarioController {
-    
+    private String alertaInclussaoSucesso;
+
     @GetMapping(value = "/usuario")
     public String telaCadastro() {
         return "usuario/cadastro";
     }
 
     @GetMapping(value = "/usuario/lista")
-    public String telaLista() {
-        List<Usuario> lista = UsuarioRepository.obterLista();
-
-        System.out.println("Usuários registrados: " + lista.size());
-
-        for(Usuario user : lista) {
-            System.out.printf("%s - %s\n", user.getNome(), user.getEmail());
-        }
+    public String telaLista(Model model) {
+        model.addAttribute("usuarios", UsuarioRepository.obterLista());
+        model.addAttribute("mensagem", alertaInclussaoSucesso);
+        alertaInclussaoSucesso = null;
 
         return "usuario/lista";
     }
 
     @PostMapping(value = "/usuario/incluir")
     public String incluir(Usuario usuario) {
-        System.out.println("Inclussão realizada!" + usuario);
-        
         UsuarioRepository.incluir(usuario);
+
+        alertaInclussaoSucesso = "Inclussão realizada com sucesso!";
 
         return "redirect:/usuario/lista";
     }
