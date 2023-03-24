61b148a68d24ac89f7b7efe128db735530196b40
LuanRibeir <luaneribeiroalmeida@gmail.com>
2023-03-24 18:01:35 -0300

    Add  persistencia de doce no db

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/domain/Doce.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/domain/Doce.java
@@ -3,8 +3,20 @@ package br.edu.infnet.appconfeitaria.model.domain;
 import br.edu.infnet.appconfeitaria.model.exceptions.NumeroDeCamadasInvalidoException;
 import br.edu.infnet.appconfeitaria.model.exceptions.NumeroDeQuantidadeInvalidoException;
 import br.edu.infnet.appconfeitaria.model.exceptions.ValorZeradoException;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "TableDoce")
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Doce {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String codigo;
     private String nome;
