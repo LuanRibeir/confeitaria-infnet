4659769532a8b77e5c963a97e434f3c58154ff55
LuanRibeir <luaneribeiroalmeida@gmail.com>
2023-03-24 16:55:53 -0300

    Change persistencia de usuario no db

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/UsuarioLoader.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/UsuarioLoader.java
@@ -3,11 +3,13 @@ package br.edu.infnet.appconfeitaria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import br.edu.infnet.appconfeitaria.model.domain.Usuario;
 import br.edu.infnet.appconfeitaria.model.service.UsuarioService;
 
+@Order(1)
 @Component
 public class UsuarioLoader implements ApplicationRunner{
 
@@ -16,10 +18,14 @@ public class UsuarioLoader implements ApplicationRunner{
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Usuario usuario = new Usuario("Administrador", "admin@admin.com", "123");
-        usuario.setId(1);
+        Usuario userAdmin = new Usuario("Administrador", "admin@admin.com", "123");
 
-        usuarioService.incluir(usuario);
+        usuarioService.incluir(userAdmin);
+
+        for (int i = 0; i < 9; i++) {
+            Usuario usuario = new Usuario("Usuario "+i, "usuario@"+i+"usuario.com", "0"+i);
+            usuarioService.incluir(usuario);
+        }
 
     }
 }
