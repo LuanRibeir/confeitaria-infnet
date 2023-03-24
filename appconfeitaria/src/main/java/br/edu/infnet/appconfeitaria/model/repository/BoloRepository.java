7f65908af9ccb40677a770e390419065cd939bf2
LuanRibeir <luaneribeiroalmeida@gmail.com>
2023-03-24 20:54:15 -0300

    Change repositorio de bolo para interface

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/repository/BoloRepository.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/repository/BoloRepository.java
@@ -1,34 +1,10 @@
 package br.edu.infnet.appconfeitaria.model.repository;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import br.edu.infnet.appconfeitaria.model.domain.Bolo;
 
 @Repository
-public class BoloRepository {
-    private static Integer id = 1;
-    private static Map<Integer,Bolo> mapaBolo = new HashMap<Integer, Bolo>();
-
-    public boolean incluir(Bolo bolo) {
-        bolo.setId(id++);
-
-        try {
-            mapaBolo.put(bolo.getId(), bolo);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    public Bolo excluir(Integer id) {
-        return mapaBolo.remove(id);
-    }
-
-    public Collection<Bolo> obterLista(){
-        return mapaBolo.values();
-    }
+public interface BoloRepository extends CrudRepository<Bolo, Integer>{
 }
