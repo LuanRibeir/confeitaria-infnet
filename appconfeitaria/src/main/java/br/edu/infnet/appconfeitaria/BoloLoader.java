8e55596c72e9705b37a624cd120b63504c34942c
LuanRibeir <luaneribeiroalmeida@gmail.com>
2023-03-24 20:53:49 -0300

    Change loader de bolo para ler txt

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/BoloLoader.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/BoloLoader.java
@@ -1,13 +1,20 @@
 package br.edu.infnet.appconfeitaria;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import br.edu.infnet.appconfeitaria.model.domain.Bolo;
 import br.edu.infnet.appconfeitaria.model.service.BoloService;
 
+
+@Order(3)
 @Component
 public class BoloLoader implements ApplicationRunner{
     @Autowired
@@ -15,9 +22,34 @@ public class BoloLoader implements ApplicationRunner{
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Bolo bolo = new Bolo("abcdefg", "Bolo de Cenoura", "Cenoura", 1, 30, "redondo", "Chocolate", 1);
-        bolo.setId(1);
+         try {
+             String arq = "src/main/resources/static/bolos.txt";
+         try {
+             FileReader fReader = new FileReader(arq);
+             BufferedReader ler = new BufferedReader(fReader);
 
-        boloService.incluir(bolo);
+             String linha = ler.readLine();
+             String[] campos = null;
+
+             while(linha != null){
+                 campos = linha.split(";");
+
+                 Bolo bolo = new Bolo(campos[0], campos[1], campos[2],
+                                     Integer.parseInt(campos[3]), Float.parseFloat(campos[4]),
+                                     campos[5], campos[6], Integer.parseInt(campos[7]));
+
+                boloService.incluir(bolo);
+
+                 linha = ler.readLine();
+             }
+
+             ler.close();
+             fReader.close();
+         } catch (IOException e) {
+             System.out.println("[ERRO] " + e.getMessage());
+         }
+         } finally {
+             System.out.println("Processamento Finalizado!");
+         }
     }
 }
