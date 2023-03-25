be5c637f3a8c678ec1bb1236bec9b2d807c2e421
LuanRibeir <luaneribeiroalmeida@gmail.com>
2023-03-25 14:28:29 -0300

    Add pedido service

---
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/service/PedidoService.java
@@ -0,0 +1,36 @@
+package br.edu.infnet.appconfeitaria.model.service;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.edu.infnet.appconfeitaria.model.domain.Pedido;
+import br.edu.infnet.appconfeitaria.model.domain.Usuario;
+import br.edu.infnet.appconfeitaria.model.repository.PedidoRepository;
+
+@Service
+public class PedidoService {
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    public Pedido incluir(Pedido Pedido) {
+        return pedidoRepository.save(Pedido); 
+    }
+
+    public void excluir(Integer key){
+        pedidoRepository.deleteById(key);
+    }
+
+    public Collection<Pedido> obterLista(){
+        return (Collection<Pedido>) pedidoRepository.findAll();
+    }
+
+    public Collection<Pedido> obterLista(Usuario usuario){
+        return (Collection<Pedido>) pedidoRepository.obterLista(usuario.getId());
+    }
+
+    public Pedido obterLista(Integer id){
+        return pedidoRepository.findById(id).orElse(null);
+    }
+}
