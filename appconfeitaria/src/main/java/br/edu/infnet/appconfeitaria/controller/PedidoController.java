a4b0c2733e31b22e76e551a175c72fc15edebc91
LuanRibeir <luaneribeiroalmeida@gmail.com>
2023-03-25 14:27:57 -0300

    Add controller de pedido

---
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/controller/PedidoController.java
@@ -0,0 +1,55 @@
+package br.edu.infnet.appconfeitaria.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import br.edu.infnet.appconfeitaria.model.domain.Pedido;
+import br.edu.infnet.appconfeitaria.model.domain.Usuario;
+import br.edu.infnet.appconfeitaria.model.service.PedidoService;
+
+@Controller
+public class PedidoController {
+    @Autowired
+    private PedidoService pedidoService;
+
+    private String msgAlerta;
+
+    @GetMapping(value = "/pedido")
+    public String telaCadastro() {
+        return "pedido/cadastro";
+    }
+
+    @GetMapping(value = "/pedido/lista")
+    public String telaLista(Model model, @SessionAttribute("usuario") Usuario usuario) {
+        model.addAttribute("pedidos", pedidoService.obterLista(usuario));
+        model.addAttribute("mensagem", msgAlerta);
+        msgAlerta = null;
+
+        return "pedido/lista";
+    }
+
+    @PostMapping(value = "/pedido/incluir")
+    public String incluir(Pedido pedido, @SessionAttribute("usuario") Usuario usuario) {
+        pedido.setUsuario(usuario);
+
+        pedidoService.incluir(pedido);
+
+        msgAlerta = "Inclussão realizada!";
+
+        return "redirect:/pedido/lista";
+    }
+
+    @GetMapping(value = "/pedido/{id}/excluir")
+    public String excluir(@PathVariable Integer id) {
+        pedidoService.excluir(id);
+
+        msgAlerta = "Exclussão realizada!";
+
+        return "redirect:/pedido/lista";
+    }
+}
