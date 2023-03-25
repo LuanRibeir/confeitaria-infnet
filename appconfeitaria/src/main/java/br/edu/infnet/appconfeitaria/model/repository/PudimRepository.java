ad2bfa63a55568f7159fc9128a51b9e7404b0aab
LuanRibeir <luaneribeiroalmeida@gmail.com>
2023-03-24 21:30:37 -0300

    Change para interface estendendo CrudRepository

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/repository/PudimRepository.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/repository/PudimRepository.java
@@ -1,34 +1,10 @@
 package br.edu.infnet.appconfeitaria.model.repository;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import br.edu.infnet.appconfeitaria.model.domain.Pudim;
 
 @Repository
-public class PudimRepository {
-    private static Integer id = 1;
-    private static Map<Integer,Pudim> mapaPudim = new HashMap<Integer, Pudim>();
-
-    public boolean incluir(Pudim pudim) {
-        pudim.setId(id++);
-
-        try {
-            mapaPudim.put(pudim.getId(), pudim);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    public Pudim excluir(Integer id) {
-        return mapaPudim.remove(id);
-    }
-
-    public Collection<Pudim> obterLista(){
-        return mapaPudim.values();
-    }
+public interface PudimRepository extends CrudRepository<Pudim, Integer>{
 }
