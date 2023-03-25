c90f5d3a5d68ec5a90af3410885ab0e6a9914875
LuanRibeir <luaneribeiroalmeida@gmail.com>
2023-03-24 21:30:34 -0300

    Change para interface estendendo CrudRepository

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/repository/TortaRepository.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/repository/TortaRepository.java
@@ -1,34 +1,10 @@
 package br.edu.infnet.appconfeitaria.model.repository;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map; 
-
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import br.edu.infnet.appconfeitaria.model.domain.Torta;
 
 @Repository
-public class TortaRepository {
-    private static Integer id = 1;
-    private static Map<Integer,Torta> mapaTorta = new HashMap<Integer, Torta>();
-
-    public boolean incluir(Torta torta) {
-        torta.setId(id++);
-
-        try {
-            mapaTorta.put(torta.getId(), torta);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    public Torta excluir(Integer id) {
-        return mapaTorta.remove(id);
-    }
-
-    public Collection<Torta> obterLista(){
-        return mapaTorta.values();
-    }
+public interface TortaRepository extends CrudRepository<Torta, Integer>{
 }
