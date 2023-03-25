289188f7a60434222a1c13726a5be68e87a200b2
LuanRibeir <luaneribeiroalmeida@gmail.com>
2023-03-25 14:28:19 -0300

    Add repositorio de pedido

---
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/model/repository/PedidoRepository.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appconfeitaria.model.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import br.edu.infnet.appconfeitaria.model.domain.Pedido;
+
+public interface PedidoRepository extends CrudRepository<Pedido, Integer>{
+    @Query("from Pedido p where p.usuario.id = :id")
+    List<Pedido> obterLista(Integer id);
+}
