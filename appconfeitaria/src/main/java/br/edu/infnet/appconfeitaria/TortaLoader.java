a05084f8ef7531f2ee7b852c49b16298526cd673
LuanRibeir <luaneribeiroalmeida@gmail.com>
2023-03-24 21:33:22 -0300

    Change loader de torta para ler txt

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/TortaLoader.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/TortaLoader.java
@@ -1,5 +1,9 @@
 package br.edu.infnet.appconfeitaria;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -15,9 +19,34 @@ public class TortaLoader implements ApplicationRunner{
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Torta torta = new Torta("abcdefg", "Torta de Morango", "Morango", 1, 40, false, "Cobertura de Morango", true);
-        torta.setId(1);
+        try {
+            String arq = "src/main/resources/static/tortas.txt";
+        try {
+            FileReader fReader = new FileReader(arq);
+            BufferedReader ler = new BufferedReader(fReader);
 
-        tortaService.incluir(torta);
-    }
+            String linha = ler.readLine();
+            String[] campos = null;
+
+            while(linha != null){
+                campos = linha.split(";");
+
+                Torta torta = new Torta(campos[0], campos[1], campos[2],
+                                    Integer.parseInt(campos[3]), Float.parseFloat(campos[4]),
+                                    Boolean.parseBoolean(campos[5]), campos[6], Boolean.parseBoolean(campos[7]));
+
+               tortaService.incluir(torta);
+
+                linha = ler.readLine();
+            }
+
+            ler.close();
+            fReader.close();
+        } catch (IOException e) {
+            System.out.println("[ERRO] " + e.getMessage());
+        }
+        } finally {
+            System.out.println("Processamento Finalizado!");
+        }
+   }
 }
