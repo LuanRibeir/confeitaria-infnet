9865978e393cfea97783076cd955fc693c33b5bf
LuanRibeir <luaneribeiroalmeida@gmail.com>
2023-03-25 16:39:46 -0300

    Change cadastro de pedido

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/controller/PedidoController.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/controller/PedidoController.java
@@ -8,25 +8,36 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
+import br.edu.infnet.appconfeitaria.model.domain.Doce;
 import br.edu.infnet.appconfeitaria.model.domain.Pedido;
 import br.edu.infnet.appconfeitaria.model.domain.Usuario;
+import br.edu.infnet.appconfeitaria.model.service.ClienteService;
+import br.edu.infnet.appconfeitaria.model.service.DoceService;
 import br.edu.infnet.appconfeitaria.model.service.PedidoService;
 
 @Controller
 public class PedidoController {
     @Autowired
     private PedidoService pedidoService;
+    @Autowired
+    private ClienteService clienteService;
+    @Autowired
+    private DoceService doceService;
 
     private String msgAlerta;
 
     @GetMapping(value = "/pedido")
-    public String telaCadastro() {
+    public String telaCadastro(Model model, @SessionAttribute("usuario") Usuario usuario) {
+        model.addAttribute("clientes", clienteService.obterLista(usuario));
+        model.addAttribute("doces", doceService.obterLista(usuario));
+
         return "pedido/cadastro";
     }
 
     @GetMapping(value = "/pedido/lista")
     public String telaLista(Model model, @SessionAttribute("usuario") Usuario usuario) {
         model.addAttribute("pedidos", pedidoService.obterLista(usuario));
+
         model.addAttribute("mensagem", msgAlerta);
         msgAlerta = null;
 
@@ -36,7 +47,6 @@ public class PedidoController {
     @PostMapping(value = "/pedido/incluir")
     public String incluir(Pedido pedido, @SessionAttribute("usuario") Usuario usuario) {
         pedido.setUsuario(usuario);
-
         pedidoService.incluir(pedido);
 
         msgAlerta = "Inclussão realizada!";
--- appconfeitaria/src/main/webapp/WEB-INF/jsp/pedido/cadastro.jsp
+++ appconfeitaria/src/main/webapp/WEB-INF/jsp/pedido/cadastro.jsp
@@ -23,6 +23,28 @@
         <label for="web">Atendimento Web:</label>
         <input type="checkbox" id="web" name="web">
       </div>
+
+	  <div class="form-group">
+		<label>Cliente</label>
+		<select name="cliente" id="form-control">
+			<c:forEach var="c" items="${clientes}">
+				<option value="${c.id}">${c.nome}</option>
+			</c:forEach>
+		</select>
+	  </div>
+
+	  <div class="form-group"> 
+		<label>Doces</label>
+		<c:forEach var="d" items="${doces}">
+			<div class="form-check">            
+				<label class="form-check-label">
+					<input type="checkbox" name="doces" value="${d.id}" class="form-check-input">
+					 ${d.nome}
+				</label>
+			</div>
+		</c:forEach>
+	  </div>
+        
 	    <button type="submit" class="btn btn-default">Cadastrar</button>
 	  </form>
 	</div>
