b108608a5581fbf082237ce1c98dbcb7659572af
LuanRibeir <luaneribeiroalmeida@gmail.com>
2023-03-24 21:33:15 -0300

    Change loader de pudim para ler txt

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/PudimLoader.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/PudimLoader.java
@@ -1,13 +1,19 @@
 package br.edu.infnet.appconfeitaria;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import br.edu.infnet.appconfeitaria.model.domain.Pudim;
 import br.edu.infnet.appconfeitaria.model.service.PudimService;
 
+@Order(4)
 @Component
 public class PudimLoader implements ApplicationRunner{
     @Autowired
@@ -15,9 +21,34 @@ public class PudimLoader implements ApplicationRunner{
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Pudim pudim = new Pudim("abcderf", "Pudim de Leite", "Leite", 1, 20, "M", false, 1);
-        pudim.setId(1);
+        try {
+            String arq = "src/main/resources/static/pudins.txt";
+        try {
+            FileReader fReader = new FileReader(arq);
+            BufferedReader ler = new BufferedReader(fReader);
 
-        pudimService.incluir(pudim);
+            String linha = ler.readLine();
+            String[] campos = null;
+
+            while(linha != null){
+                campos = linha.split(";");
+
+                Pudim pudim = new Pudim(campos[0], campos[1], campos[2],
+                                    Integer.parseInt(campos[3]), Float.parseFloat(campos[4]),
+                                    campos[5], Boolean.parseBoolean(campos[6]), Integer.parseInt(campos[7]));
+
+               pudimService.incluir(pudim);
+
+                linha = ler.readLine();
+            }
+
+            ler.close();
+            fReader.close();
+        } catch (IOException e) {
+            System.out.println("[ERRO] " + e.getMessage());
+        }
+        } finally {
+            System.out.println("Processamento Finalizado!");
+        }
     }
 }
