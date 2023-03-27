12b9d205ce7a569ad110730325c342b34fb876f8
LuanRibeir <luaneribeiroalmeida@gmail.com>
2023-03-27 16:30:10 -0300

    Enable dependencia

--- appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/AppconfeitariaApplication.java
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/AppconfeitariaApplication.java
@@ -2,8 +2,10 @@ package br.edu.infnet.appconfeitaria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AppconfeitariaApplication {
 
 	public static void main(String[] args) {
