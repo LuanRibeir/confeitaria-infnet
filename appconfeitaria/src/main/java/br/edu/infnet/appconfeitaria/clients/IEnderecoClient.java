31ece335e3a1df9f995712d4a96f80aa9ea8243b
LuanRibeir <luaneribeiroalmeida@gmail.com>
2023-03-27 16:27:58 -0300

    Add interface de endereco

---
+++ appconfeitaria/src/main/java/br/edu/infnet/appconfeitaria/clients/IEnderecoClient.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appconfeitaria.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import br.edu.infnet.appconfeitaria.model.domain.Endereco;
+
+@FeignClient(url = "https://viacep.com.br/ws", name = "enderecoClient")
+public interface IEnderecoClient {
+    @GetMapping(value = "/{cep}/json")
+    public Endereco buscaPorCep(@PathVariable String cep);
+}
